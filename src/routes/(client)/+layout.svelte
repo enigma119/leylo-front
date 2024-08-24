f655dd097c728d177598f34fd41c4486187fc693
Abass SARR <abasssarr0@gmail.com>
2024-08-24 19:46:03 +0000

    adding flowing  UI

---
+++ src/routes/(client)/+layout.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import Header from '../Header.svelte';
+    import type { LayoutData } from './$types';
+	import '../app.css';
+
+    
+    export let data: LayoutData;
+</script>
+
+<div class="app">
+	<Header></Header>
+
+	<main>
+		<slot></slot>
+	</main>
+
+	<footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer>
+</div>
+
+
+<style>
+    .app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+    footer {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 12px;
+    }
+    
+    footer a {
+        font-weight: bold;
+    }
+
+</style>
